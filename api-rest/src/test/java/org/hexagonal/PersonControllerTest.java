88cded25a881b3fe860cf652cce94635325e398e
Luis Rojo <luiso54@gmail.com>
2023-11-22 13:04:59 +0100

    feat: jacoco configured, versions centralized in parent pom, added api yml example

---
+++ api-rest/src/test/java/org/hexagonal/PersonControllerTest.java
@@ -0,0 +1,47 @@
+package org.hexagonal;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import org.hexagonal.openapi.model.Person;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@ExtendWith(MockitoExtension.class)
+class PersonControllerTest {
+
+    @InjectMocks
+    private PersonController underTest;
+
+    private final ObjectMapper objectMapper = new ObjectMapper();
+
+    private MockMvc mockMvc;
+
+    @BeforeEach
+    void setup() {
+        objectMapper.registerModule(new JavaTimeModule());
+        mockMvc = MockMvcBuilders.standaloneSetup(underTest).build();
+    }
+
+    @Test
+    void personTest() throws Exception {
+
+        final ResultActions mvcResult = mockMvc
+                .perform(MockMvcRequestBuilders.get("/multimodule-hexagonal/person"));
+
+        final Person result = objectMapper.readValue(
+                mvcResult.andReturn().getResponse().getContentAsString(), Person.class);
+
+        assertThat(result.getId()).isEqualTo(1);
+        assertThat(result.getName()).hasToString("Luis");
+
+    }
+}
